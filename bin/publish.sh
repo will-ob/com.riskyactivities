3eb25bf5b069539c2641c741300954b56eb36d76
Will O'Brien <will.obrien@dibit.co>
2013-09-14 17:40:02 -0400

    stash appropriately

--- bin/publish.sh
+++ bin/publish.sh
@@ -10,9 +10,11 @@ grunt build || { echo 'Build failed'; exit 1; }
 
 # checkout new branch
 WORKING_BRANCH="$(git rev-parse --abbrev-ref HEAD)"
-IS_STASHED=[ $(git stash) = "No local changes to save" ]
+
+# if there is unsaved work, stash it
+test -n "$(git status --porcelain)" && $(git stash save --include-untracked) && stashed=true
+
 git checkout --orphan build
-git reset --hard $WORKING_BRANCH
 
 # add all files in the dist dir to branch at root level
 FILES="$(ls dist)"
@@ -20,15 +22,16 @@ FILES="$(ls dist)"
 mv dist/* ./ -b
 
 git rm -rf .
-for file in "$FILES"; do
-  git add $file -f
-  if [ -e $file~ ]; then
-    if [ -d $file ]; then
-      rm -rf $file
+for file in $FILES; do
+  git add "$file" -f
+  echo "$file"
+  if [ -e "$file~" ]; then
+    if [ -d "$file" ]; then
+      rm -rf "$file"
     else
-      rm $file
+      rm "$file"
     fi
-    mv $file~ $file
+    mv "$file~" "$file"
   fi
 done
 
@@ -38,7 +41,11 @@ git commit -m "Built $(date) : $(git log | head -1)"
 git push origin build:gh-pages -f
 
 # return to working branch
+git reset --hard $WORKING_BRANCH
 git checkout $WORKING_BRANCH
-git reset --hard HEAD
-if [ $IS_STASHED ]; then git stash pop; fi
+
+# if there wast unsaved work, restore it
+if $stashed; then git stash pop; fi
+
 git branch -D build
+
