1849d5019628b29b431e71297d57fa6dc1341680
Will O'Brien <will.obrien@dibit.co>
2013-09-14 10:22:46 -0400

    git clean and restore directories

--- bin/publish.sh
+++ bin/publish.sh
@@ -2,14 +2,15 @@
 
 # directory of this script
 DIR="$( cd "$( dirname "$0" )" && pwd )"
-cd ../$DIR
+echo "$DIR"
+cd $DIR/../
 
 # build
 grunt build || { echo 'Build failed'; exit 1; }
 
 # checkout new branch
 WORKING_BRANCH="$(git rev-parse --abbrev-ref HEAD)"
-git stash
+IS_STASHED=[ $(git stash) = "No local changes to save" ]
 git checkout --orphan build
 git reset --hard $WORKING_BRANCH
 
@@ -18,11 +19,15 @@ FILES="$(ls dist)"
 
 mv dist/* ./ -b
 
-git rm -rf
+git rm -rf .
 for file in "$FILES"; do
   git add $file -f
   if [ -e $file~ ]; then
-    rm $file
+    if [ -d $file ]; then
+      rm -rf $file
+    else
+      rm $file
+    fi
     mv $file~ $file
   fi
 done
@@ -35,5 +40,5 @@ git push origin build:gh-pages -f
 # return to working branch
 git checkout $WORKING_BRANCH
 git reset --hard HEAD
+if [ $IS_STASHED ]; then git stash pop; fi
 git branch -D build
-git stash pop
